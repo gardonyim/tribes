50d60701a64d4f915387bbc88c900c033c6b03b7
nagymihalyi <rubin.schwarz@freemail.hu>
2022-04-20 00:57:22 +0200

    Add Lombok to build.gradle and use it in Building entity

--- src/main/java/com/greenfoxacademy/springwebapp/building/models/Building.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/models/Building.java
@@ -2,7 +2,9 @@ package com.greenfoxacademy.springwebapp.building.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity(name = "buildings")
 public class Building {
   @Id
@@ -11,54 +13,4 @@ public class Building {
   private int level;
   private int hp;
   private int finishedAt;
-
-  public Building(BuildingType buildingType, int level, int hp, int finishedAt) {
-    this.buildingType = buildingType;
-    this.level = level;
-    this.hp = hp;
-    this.finishedAt = finishedAt;
-  }
-
-  public Building() {
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public BuildingType getBuildingType() {
-    return buildingType;
-  }
-
-  public void setBuildingType(BuildingType type) {
-    this.buildingType = type;
-  }
-
-  public int getLevel() {
-    return level;
-  }
-
-  public void setLevel(int level) {
-    this.level = level;
-  }
-
-  public int getHp() {
-    return hp;
-  }
-
-  public void setHp(int hp) {
-    this.hp = hp;
-  }
-
-  public int getFinishedAt() {
-    return finishedAt;
-  }
-
-  public void setFinishedAt(int finishedAt) {
-    this.finishedAt = finishedAt;
-  }
 }
