a88186fa5f70076f0cdda1ac3f67481e9a2ed70e
nagymihalyi <rubin.schwarz@freemail.hu>
2022-04-20 12:37:38 +0200

    Fixing Id autoincrement.

--- src/main/java/com/greenfoxacademy/springwebapp/building/models/Building.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/models/Building.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.springwebapp.building.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Data;
 
@@ -8,9 +10,11 @@ import lombok.Data;
 @Entity(name = "buildings")
 public class Building {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private BuildingType buildingType;
   private int level;
   private int hp;
+  private int startedAt;
   private int finishedAt;
 }
