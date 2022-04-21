852ee608ddaa5fe432e82c4cda3f1a9c06f2021e
unknown <s.gabor1988@gmail.com>
2022-04-21 08:39:56 +0200

    refactor(create resource): Move LocalDateTimeConverter into utilities package

--- src/main/java/com/greenfoxacademy/springwebapp/utilities/LocalDateTimeConverter.java
+++ src/main/java/com/greenfoxacademy/springwebapp/utilities/LocalDateTimeConverter.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy.springwebapp.resource.models;
+package com.greenfoxacademy.springwebapp.utilities;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
