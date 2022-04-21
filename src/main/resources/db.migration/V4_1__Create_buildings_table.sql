b78e9587e7c3074f55d765344f0338623b4bc028
nagymihalyi <rubin.schwarz@freemail.hu>
2022-04-21 18:33:11 +0200

    Removing a wrong sql script file

--- src/main/resources/db.migration/V4_1__Create_buildings_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE buildings (
-                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                         building_type VARCHAR(32) NOT NULL,
-                         level INT NOT NULL UNSIGNED,
-                         hp INT NOT NULL UNSIGNED,
-                         startedAt INT NOT NULL,
-                         finishedAt INT
-);
