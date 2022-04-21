4e26e97dfd5003188d708d7e2609c75be8edb33e
unknown <s.gabor1988@gmail.com>
2022-04-21 22:17:18 +0200

    feat(Security): add PlayerTokenDTO

---
+++ src/main/java/com/greenfoxacademy/springwebapp/player/models/PlayerTokenDTO.java
@@ -0,0 +1,51 @@
+package com.greenfoxacademy.springwebapp.player.models;
+
+public class PlayerTokenDTO {    private int id;
+  private String username;
+  private int kingdomId;
+  private String kingdomName;
+
+  public PlayerTokenDTO() {
+  }
+
+  public PlayerTokenDTO(int id, String username, int kingdomId, String kingdomName, String avatar, Integer points) {
+    this.id = id;
+    this.username = username;
+    this.kingdomId = kingdomId;
+    this.kingdomName = kingdomName;
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public int getKingdomId() {
+    return kingdomId;
+  }
+
+  public void setKingdomId(int kingdomId) {
+    this.kingdomId = kingdomId;
+  }
+
+  public String getKingdomName() {
+    return kingdomName;
+  }
+
+  public void setKingdomName(String kingdomName) {
+    this.kingdomName = kingdomName;
+  }
+
+}
