7eaa027f0c01f434f0cc4a5365e6e9b55c9979b7
nagymihalyi <rubin.schwarz@freemail.hu>
2022-04-21 20:20:20 +0200

    Fixing conflicts

--- src/main/java/com/greenfoxacademy/springwebapp/building/models/BuildingType.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/models/BuildingType.java
@@ -1,11 +1,14 @@
 package com.greenfoxacademy.springwebapp.building.models;
 
 public enum BuildingType {
-<<<<<<< HEAD
-  TOWNHALL("townhall", 200),
-  MINE("mine", 100),
-  FARM("farm", 100),
-  ACADEMY("academy", 150);
+
+  TOWNHALL("townhall",200),
+
+  MINE("mine",100),
+
+  FARM("farm",100),
+
+  ACADEMY("academy",150);
 
   private final String name;
   private final int hpParameter;
@@ -18,11 +21,8 @@ public enum BuildingType {
   public String getName() {
     return name;
   }
-<<<<<<< HEAD
 
   public int getHpParameter() {
     return hpParameter;
   }
-=======
->>>>>>> f951a4494fd1c894b52377851e09ee94280121d1
 }
