3688199484f92e226dc2d85987d7e0497244d040
unknown <s.gabor1988@gmail.com>
2022-04-21 07:03:10 +0200

    feat(create resource): ResourceRepository

---
+++ src/main/java/com/greenfoxacademy/springwebapp/resource/ResourceRepository.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.springwebapp.resource;
+
+import com.greenfoxacademy.springwebapp.resource.models.Resource;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResourceRepository extends CrudRepository<Resource, Integer> {
+
+  Resource save(Resource resource);
+}
