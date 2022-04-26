310b779b84ddae642c796d9bf526e5c54e068184
unknown <s.gabor1988@gmail.com>
2022-04-26 18:01:17 +0200

    feat(Get-Resources-Endpoint): add new DTO converter methods to service

--- src/main/java/com/greenfoxacademy/springwebapp/resource/ResourceService.java
+++ src/main/java/com/greenfoxacademy/springwebapp/resource/ResourceService.java
@@ -1,8 +1,15 @@
 package com.greenfoxacademy.springwebapp.resource;
 
 import com.greenfoxacademy.springwebapp.resource.models.Resource;
+import com.greenfoxacademy.springwebapp.resource.models.ResourceDTO;
+
+import java.util.List;
 
 public interface ResourceService {
   Resource save(Resource resource);
+
+  ResourceDTO convertToResourceDTO(Resource resource);
+
+  List<ResourceDTO> convertToResourceDtoList(List<Resource> resourceList);
 
 }
