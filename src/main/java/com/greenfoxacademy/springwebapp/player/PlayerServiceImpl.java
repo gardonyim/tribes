bd139a49686a3f221504a6a7a42b1b85ae062f97
unknown <s.gabor1988@gmail.com>
2022-04-26 12:00:18 +0200

    fix(Security): fixies because merge conflicts

--- src/main/java/com/greenfoxacademy/springwebapp/player/PlayerServiceImpl.java
+++ src/main/java/com/greenfoxacademy/springwebapp/player/PlayerServiceImpl.java
@@ -1,8 +1,14 @@
 package com.greenfoxacademy.springwebapp.player;
 
+import com.greenfoxacademy.springwebapp.exceptions.RequestParameterMissingException;
+import com.greenfoxacademy.springwebapp.exceptions.RequestNotAcceptableException;
+import com.greenfoxacademy.springwebapp.exceptions.RequestCauseConflictException;
+import com.greenfoxacademy.springwebapp.kingdom.KingdomService;
 import com.greenfoxacademy.springwebapp.player.models.Player;
 import com.greenfoxacademy.springwebapp.player.models.RegistrationReqDTO;
+import com.greenfoxacademy.springwebapp.player.models.RegistrationResDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,15 +17,60 @@ import java.util.Optional;
 public class PlayerServiceImpl implements PlayerService {
 
   private PlayerRepository playerRepository;
+  private KingdomService kingdomService;
+  private PasswordEncoder pwEnc;
 
   @Autowired
-  public PlayerServiceImpl(PlayerRepository playerRepository) {
+  public void setPlayerRepository(PlayerRepository playerRepository) {
     this.playerRepository = playerRepository;
   }
 
+  @Autowired
+  public void setKingdomService(KingdomService kingdomService) {
+    this.kingdomService = kingdomService;
+  }
+
+  @Autowired
+  public void setPwEnc(PasswordEncoder pwEnc) {
+    this.pwEnc = pwEnc;
+  }
+
   @Override
-  public Player savePlayer(RegistrationReqDTO reqDTO) {
-    return playerRepository.save(new Player(reqDTO.getUsername(), "", null, "", 0));
+  public RegistrationResDTO savePlayer(RegistrationReqDTO reqDTO) {
+    validateRegistration(reqDTO);
+    Player player = playerRepository.save(convert(reqDTO));
+    player.setKingdom(kingdomService.save(reqDTO.getKingdomname(), player));
+    return new RegistrationResDTO(player);
+  }
+
+  private void validateRegistration(RegistrationReqDTO reqDTO) {
+    if (reqDTO.getPassword() == null || reqDTO.getPassword().trim().isEmpty()) {
+      if (reqDTO.getUsername() == null || reqDTO.getUsername().trim().isEmpty()) {
+        throw new RequestParameterMissingException("Username and password are required.");
+      } else {
+        throw new RequestParameterMissingException("Password is required.");
+      }
+    }
+    if (reqDTO.getUsername() == null || reqDTO.getUsername().trim().isEmpty()) {
+      throw new RequestParameterMissingException("Username is required.");
+    }
+    int minPasswordLength = 8;
+    if (reqDTO.getPassword().trim().length() < minPasswordLength) {
+      throw new RequestNotAcceptableException("Password must be at least 8 characters.");
+    }
+    if (playerRepository.findFirstByUsername(reqDTO.getUsername()).isPresent()) {
+      throw new RequestCauseConflictException("Username is already taken.");
+    }
+  }
+
+  private Player convert(RegistrationReqDTO reqDTO) {
+    Player player = new Player();
+    player.setUsername(reqDTO.getUsername());
+    player.setPassword(pwEnc.encode(reqDTO.getPassword().trim()));
+    player.setKingdom(null);
+    player.setAvatar("");
+    player.setPoints(0);
+    return player;
   }
 
   @Override
@@ -27,6 +78,5 @@ public class PlayerServiceImpl implements PlayerService {
     Optional<Player> player = playerRepository.findFirstByUsername(username);
     return player;
   }
-
 
 }
