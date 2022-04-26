9892924b4f9c72c4c47c515e8b9952d1cce00e18
unknown <s.gabor1988@gmail.com>
2022-04-26 18:01:17 +0200

    feat(Get-Resources-Endpoint): add new DTO converter methods to service

--- src/main/java/com/greenfoxacademy/springwebapp/resource/ResourceServiceImpl.java
+++ src/main/java/com/greenfoxacademy/springwebapp/resource/ResourceServiceImpl.java
@@ -1,8 +1,13 @@
 package com.greenfoxacademy.springwebapp.resource;
 
 import com.greenfoxacademy.springwebapp.resource.models.Resource;
+import com.greenfoxacademy.springwebapp.resource.models.ResourceDTO;
+import com.greenfoxacademy.springwebapp.utilities.EpochConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ResourceServiceImpl implements ResourceService {
@@ -17,6 +22,26 @@ public class ResourceServiceImpl implements ResourceService {
   @Override
   public Resource save(Resource resource) {
     return resourceRepository.save(resource);
+  }
+
+  @Override
+  public ResourceDTO convertToResourceDTO(Resource resource) {
+    if (resource == null) {
+      return null;
+    }
+    return new ResourceDTO(
+        resource.getResourceType().getDescription(),
+        resource.getAmount(),
+        resource.getGeneration(),
+        EpochConverter.convertToEpochMilli(resource.getUpdatedAt())
+    );
+  }
+
+  @Override
+  public List<ResourceDTO> convertToResourceDtoList(List<Resource> resourceList) {
+    return resourceList.stream()
+        .map(this::convertToResourceDTO)
+        .collect(Collectors.toList());
   }
 
 }
