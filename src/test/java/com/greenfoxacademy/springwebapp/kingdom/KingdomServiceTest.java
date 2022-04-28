ff2b0d8bdf84297452af2cc49c57243c95710d0d
nagymihalyi <rubin.schwarz@freemail.hu>
2022-04-29 01:05:50 +0200

    Adding test

--- src/test/java/com/greenfoxacademy/springwebapp/kingdom/KingdomServiceTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/kingdom/KingdomServiceTest.java
@@ -1,7 +1,9 @@
 package com.greenfoxacademy.springwebapp.kingdom;
 
+import com.greenfoxacademy.springwebapp.building.models.BuildingType;
 import com.greenfoxacademy.springwebapp.kingdom.models.Kingdom;
 import com.greenfoxacademy.springwebapp.player.models.Player;
+import com.greenfoxacademy.springwebapp.resource.models.ResourceType;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,5 +52,25 @@ public class KingdomServiceTest {
     Kingdom created = kingdomService.save("testkingdom", player);
 
     Assert.assertEquals(player, created.getPlayer());
+  }
+
+  @Test
+  public void when_saveKingdom_should_returnKingdomWithGeneratedDefaultPack() {
+    Player player = new Player("testuser", "testpassword", null, "", 0);
+    when(kingdomRepository.save(any(Kingdom.class))).then(returnsFirstArg());
+
+    Kingdom testKingdomWithDefaultPack = kingdomService.save("testkingdom", player);
+
+    Assert.assertEquals(4, testKingdomWithDefaultPack.getBuildings().size());
+    Assert.assertEquals(BuildingType.TOWNHALL, testKingdomWithDefaultPack.getBuildings().get(0).getBuildingType());
+    Assert.assertEquals(BuildingType.FARM, testKingdomWithDefaultPack.getBuildings().get(1).getBuildingType());
+    Assert.assertEquals(BuildingType.MINE, testKingdomWithDefaultPack.getBuildings().get(2).getBuildingType());
+    Assert.assertEquals(BuildingType.ACADEMY, testKingdomWithDefaultPack.getBuildings().get(3).getBuildingType());
+
+    Assert.assertEquals(2, testKingdomWithDefaultPack.getResources().size());
+    Assert.assertEquals(ResourceType.FOOD, testKingdomWithDefaultPack.getResources().get(0).getResourceType());
+    Assert.assertEquals(ResourceType.GOLD, testKingdomWithDefaultPack.getResources().get(1).getResourceType());
+    Assert.assertEquals(1000, testKingdomWithDefaultPack.getResources().get(0).getAmount());
+    Assert.assertEquals(1000, testKingdomWithDefaultPack.getResources().get(1).getAmount());
   }
 }
