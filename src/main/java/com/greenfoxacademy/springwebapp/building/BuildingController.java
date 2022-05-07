737bfa31458ba1a1d390ab23228ceb9e801fa6fb
unknown <s.gabor1988@gmail.com>
2022-05-07 23:28:45 +0200

    feat(Put Buildings): add new put endpoint method for modify the bulding level

--- src/main/java/com/greenfoxacademy/springwebapp/building/BuildingController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/BuildingController.java
@@ -47,5 +47,4 @@ public class BuildingController {
     return ResponseEntity.status(200).body(buildingService.provideDtoAboutBuildingDevResoult(
             buildingDTO, ((Player) user.getPrincipal()).getKingdom(), buildingId));
   }
-
 }
