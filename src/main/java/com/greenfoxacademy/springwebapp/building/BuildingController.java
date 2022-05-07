e0936637a2ff15ca96c35b755423ee3421df0634
unknown <s.gabor1988@gmail.com>
2022-05-07 23:28:45 +0200

    feat(Put Buildings): add new put endpoint method for modify the bulding level

--- src/main/java/com/greenfoxacademy/springwebapp/building/BuildingController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/BuildingController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
