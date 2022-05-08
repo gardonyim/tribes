e9479c280d59491c8841a4cb5eae4eef06883385
nagymihalyi <rubin.schwarz@freemail.hu>
2022-05-08 15:13:21 +0200

    Create new endpoints in BuildingController

--- src/main/java/com/greenfoxacademy/springwebapp/building/BuildingController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/BuildingController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.springwebapp.building;
 
+import com.greenfoxacademy.springwebapp.building.models.Building;
+import com.greenfoxacademy.springwebapp.building.models.BuildingDTO;
 import com.greenfoxacademy.springwebapp.building.models.BuildingTypeDTO;
 import com.greenfoxacademy.springwebapp.building.services.BuildingService;
 import com.greenfoxacademy.springwebapp.kingdom.models.Kingdom;
@@ -8,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/kingdom/buildings")
@@ -23,7 +27,20 @@ public class BuildingController {
     this.buildingService = buildingService;
   }
 
-  @PostMapping
+  @GetMapping("/kingdom/buildings")
+  public ResponseEntity getBuildingsDTO(UsernamePasswordAuthenticationToken user) {
+    Kingdom kingdom = ((Player) user.getPrincipal()).getKingdom();
+    return ResponseEntity.status(200).body(buildingService.getBuildingDtoList(kingdom));
+  }
+
+  @GetMapping("/kingdom/buildings/{id}")
+  public ResponseEntity<BuildingDTO> getBuildingDTO (UsernamePasswordAuthenticationToken user,
+    @RequestParam (required = false) Integer id) {
+    Kingdom kingdom = ((Player) user.getPrincipal()).getKingdom();
+    return ResponseEntity.status(200).body(buildingService.getBuildingDTO(id, kingdom));
+    }
+
+    @PostMapping
   public ResponseEntity addBuilding(UsernamePasswordAuthenticationToken user,
                                     @RequestBody BuildingTypeDTO typeDTO) {
     Kingdom kingdom = ((Player) user.getPrincipal()).getKingdom();
