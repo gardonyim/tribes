d5edc1be24b4fc8d9a30b54017dd50b31c3962e3
unknown <s.gabor1988@gmail.com>
2022-05-08 19:49:03 +0200

    test(Get Kingdom): create unit test for converter methodes of KingdomService

--- src/test/java/com/greenfoxacademy/springwebapp/kingdom/KingdomServiceTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/kingdom/KingdomServiceTest.java
@@ -1,17 +1,31 @@
 package com.greenfoxacademy.springwebapp.kingdom;
 
 import com.greenfoxacademy.springwebapp.building.models.Building;
+import com.greenfoxacademy.springwebapp.building.models.BuildingDTO;
 import com.greenfoxacademy.springwebapp.building.models.BuildingType;
 import com.greenfoxacademy.springwebapp.building.services.BuildingServiceImpl;
 import com.greenfoxacademy.springwebapp.kingdom.models.Kingdom;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomBaseDTO;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomResFullDTO;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomResWrappedDTO;
 import com.greenfoxacademy.springwebapp.location.LocationService;
 import com.greenfoxacademy.springwebapp.location.models.Location;
+import com.greenfoxacademy.springwebapp.location.models.LocationDTO;
 import com.greenfoxacademy.springwebapp.player.models.Player;
 import com.greenfoxacademy.springwebapp.resource.ResourceServiceImpl;
 import com.greenfoxacademy.springwebapp.resource.models.Resource;
+import com.greenfoxacademy.springwebapp.resource.models.ResourceDTO;
 import com.greenfoxacademy.springwebapp.resource.models.ResourceType;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.greenfoxacademy.springwebapp.troop.TroopServiceImpl;
+import com.greenfoxacademy.springwebapp.troop.models.Troop;
+import com.greenfoxacademy.springwebapp.troop.models.dtos.TroopDTO;
+import com.greenfoxacademy.springwebapp.utilities.TimeService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +48,8 @@ public class KingdomServiceTest {
   ResourceServiceImpl resourceService;
   @Mock
   LocationService locationService;
+  @Mock
+  TroopServiceImpl troopService;
   @InjectMocks
   KingdomServiceImpl kingdomService;
 
@@ -97,6 +113,72 @@ public class KingdomServiceTest {
     Assert.assertEquals(ResourceType.GOLD, testKingdomWithDefaultPack.getResources().get(1).getResourceType());
     Assert.assertEquals(1000, testKingdomWithDefaultPack.getResources().get(0).getAmount());
     Assert.assertEquals(1000, testKingdomWithDefaultPack.getResources().get(1).getAmount());
+  }
+
+  @Test
+  public void when_convertKingdom_should_returnProperKingdomResFullDTO() {
+    LocalDateTime ldt = LocalDateTime.now();
+    long ldte = TimeService.toEpochSecond(ldt);
+    Kingdom kdm = createTestKingdom(ldt);
+    when(buildingService.convertToDTO(any())).thenReturn(new BuildingDTO(0,
+        kdm.getBuildings().get(0).getBuildingType(), kdm.getBuildings().get(0).getLevel(),
+        kdm.getBuildings().get(0).getHp(), ldte, ldte));
+    when(resourceService.convertToResourceDTO((any())))
+        .thenReturn(new ResourceDTO("gold", 100, 1, ldte));
+    when(troopService.convert((any()))).thenReturn(new TroopDTO(null, 1,1,1,1, ldte, ldte));
+    when(locationService.convertToLocationDTO((any())))
+        .thenReturn(new LocationDTO(kdm.getLocation().getxcoordinate(), kdm.getLocation().getxcoordinate()));
+    KingdomResFullDTO expectedKingdomResFullDTO = createExpectedKingdomResFullDTO(kdm);
+    KingdomResFullDTO actualKingdomResFullDTO = kingdomService.convertToKingdomResFullDTO(kdm);
+    Assert.assertEquals(expectedKingdomResFullDTO, actualKingdomResFullDTO);
+  }
+
+  @Test
+  public void when_convertKingdom_should_returnProperKingdomResWrappedDTO() {
+    Kingdom kdm = createTestKingdom(null);
+    when(locationService.convertToLocationDTO((any())))
+        .thenReturn(new LocationDTO(kdm.getLocation().getxcoordinate(), kdm.getLocation().getxcoordinate()));
+    KingdomResWrappedDTO expectedKingdomResWrappedDTO = createExpectedKingdomResWrappedDTO(kdm);
+    KingdomResWrappedDTO actualKingdomResWrappedDTO = kingdomService.convertToKingdomResWrappedDTO(kdm);
+    Assert.assertEquals(expectedKingdomResWrappedDTO, actualKingdomResWrappedDTO);
+  }
+
+  private Kingdom createTestKingdom(LocalDateTime ldt) {
+    Kingdom kdm = new Kingdom();
+    kdm.setPlayer(new Player(1, "Bela", "verysecret", kdm, "", 100));
+    kdm.setBuildings(new ArrayList<>(Arrays.asList(new Building(BuildingType.FARM, 1, kdm, ldt, ldt))));
+    kdm.setResources(new ArrayList<>(Arrays.asList(new Resource(ResourceType.GOLD, 100, 1, ldt, kdm))));
+    kdm.setTroops(new ArrayList<>(Arrays.asList(new Troop(1, 1, 1, 1, ldt, ldt, kdm))));
+    kdm.setName("Magyarisztan");
+    kdm.setId(1);
+    kdm.setLocation(new Location(1, 100, 100));
+    return kdm;
+  }
+
+  private KingdomResFullDTO createExpectedKingdomResFullDTO(Kingdom testKingdom) {
+    long ldt = TimeService.toEpochSecond(testKingdom.getBuildings().get(0).getFinishedAt());
+    KingdomResFullDTO expected = new KingdomResFullDTO();
+    expected.setKingdomId(testKingdom.getId());
+    expected.setName(testKingdom.getName());
+    expected.setUserId(testKingdom.getId());
+    expected.setLocation(
+        new LocationDTO(testKingdom.getLocation().getxcoordinate(), testKingdom.getLocation().getxcoordinate()));
+    expected.setBuildings(Arrays.asList(new BuildingDTO(
+        0, testKingdom.getBuildings().get(0).getBuildingType(),
+        testKingdom.getBuildings().get(0).getLevel(), testKingdom.getBuildings().get(0).getHp(), ldt, ldt)));
+    expected.setResources(Arrays.asList(new ResourceDTO("gold", 100, 1, ldt)));
+    expected.setTroops(Arrays.asList(new TroopDTO(null, 1,1,1,1, ldt, ldt)));
+    return expected;
+  }
+
+  private KingdomResWrappedDTO createExpectedKingdomResWrappedDTO(Kingdom testKingdom) {
+    KingdomBaseDTO kingdomBaseDTO = new KingdomBaseDTO(
+        testKingdom.getId(),
+        testKingdom.getName(),
+        testKingdom.getId(),
+        new LocationDTO(testKingdom.getLocation().getxcoordinate(), testKingdom.getLocation().getxcoordinate())
+    );
+    return new KingdomResWrappedDTO(kingdomBaseDTO);
   }
 
   private List<Building> buildingListCreator() {
