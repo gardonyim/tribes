05abd7b182b023715d25d59d9c15108da37d0f94
unknown <s.gabor1988@gmail.com>
2022-05-08 23:23:21 +0200

    fix(Get Kingdom): fix get kingdom endpoint

--- src/main/java/com/greenfoxacademy/springwebapp/kingdom/KingdomController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/kingdom/KingdomController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springwebapp.kingdom;
 
+import com.greenfoxacademy.springwebapp.exceptions.ForbiddenActionException;
 import com.greenfoxacademy.springwebapp.player.models.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,18 +20,20 @@ public class KingdomController {
   }
 
   @GetMapping({"/kingdom","/kingdom/{id}"})
-  public ResponseEntity getKingdom(@PathVariable(required = false, value = "id") Integer kingdomId,
+  public ResponseEntity getKingdom(@PathVariable(required = false, value = "id") String kingdomId,
                                    UsernamePasswordAuthenticationToken user) {
     Player player = (Player)user.getPrincipal();
     int myKingdomId = player.getKingdom().getId();
-    if (kingdomId == null || kingdomId == myKingdomId) {
+    if (kingdomId == null || (kingdomId.matches("\\d+") && (Integer.valueOf(kingdomId) == myKingdomId))) {
       return ResponseEntity
           .status(200)
           .body(kingdomService.convertToKingdomResFullDTO(player.getKingdom()));
-    } else {
+    } else if (kingdomId.matches("\\d+")) {
       return ResponseEntity
           .status(200)
-          .body(kingdomService.convertToKingdomResWrappedDTO(player.getKingdom()));
+          .body(kingdomService.convertToKingdomResWrappedDTO(kingdomService.findById(Integer.valueOf(kingdomId))));
+    } else {
+      throw new ForbiddenActionException();
     }
 
   }
