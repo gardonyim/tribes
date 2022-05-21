f8bea19918d387f98da1bc58518591889676068d
unknown <s.gabor1988@gmail.com>
2022-05-21 23:15:58 +0200

    refactor(Put Building): refactor some method in connection with building level modification

--- src/main/java/com/greenfoxacademy/springwebapp/building/services/BuildingServiceImpl.java
+++ src/main/java/com/greenfoxacademy/springwebapp/building/services/BuildingServiceImpl.java
@@ -95,7 +95,12 @@ public class BuildingServiceImpl implements BuildingService {
   }
 
   @Override
-  public BuildingDTO modifyBuildingLevel(BuildingDTO buildingDTO, Kingdom kingdom, Integer buildingId) {
+  public BuildingDTO provideDtoAboutBuildingDevResoult(BuildingDTO buildingDTO, Kingdom kingdom, Integer buildingId) {
+    return convertToDTO(modifyBuildingLevel(buildingDTO, kingdom, buildingId));
+  }
+
+  @Override
+  public Building modifyBuildingLevel(BuildingDTO buildingDTO, Kingdom kingdom, Integer buildingId) {
     Building modifiableBuilding = validateModifyBuildingLevelRequest(buildingDTO, kingdom, buildingId);
     int reqBuildingLevel = (buildingDTO == null) ? modifiableBuilding.getLevel() + 1 : buildingDTO.getLevel();
     modifiableBuilding.setLevel(reqBuildingLevel);
@@ -110,7 +115,7 @@ public class BuildingServiceImpl implements BuildingService {
         modifiableBuilding.getBuildingType().getName().toLowerCase(), modifiableBuilding.getLevel(), reqBuildingLevel),
         modifiableBuilding.getStartedAt()));
     kingdomService.update(kingdom);
-    return convertToDTO(modifiableBuilding);
+    return modifiableBuilding;
   }
 
   @Override
@@ -133,7 +138,7 @@ public class BuildingServiceImpl implements BuildingService {
     return modifiableBuilding;
   }
 
-  private void validateHasEnoughGold(Building modifiableBuilding, int reqBuildingLevel) {
+  public void validateHasEnoughGold(Building modifiableBuilding, int reqBuildingLevel) {
     if (resourceService.hasEnoughGold(modifiableBuilding.getKingdom(),
         gameObjectRuleHolder.calcCreationCost(modifiableBuilding.getBuildingType().getName().toLowerCase(),
             modifiableBuilding.getLevel(), reqBuildingLevel))) {
