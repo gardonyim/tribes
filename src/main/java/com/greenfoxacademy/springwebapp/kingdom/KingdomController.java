5d18bf1618aa36daa91f70f108effa34a90ab8ab
unknown <s.gabor1988@gmail.com>
2022-05-21 11:33:03 +0200

    refactor(Get Kingdom): split the Get endpoint into two more specizalized

--- src/main/java/com/greenfoxacademy/springwebapp/kingdom/KingdomController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/kingdom/KingdomController.java
@@ -1,15 +1,18 @@
 package com.greenfoxacademy.springwebapp.kingdom;
 
-import com.greenfoxacademy.springwebapp.kingdom.models.Kingdom;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomResFullDTO;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomResWrappedDTO;
 import com.greenfoxacademy.springwebapp.player.models.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kingdom")
 public class KingdomController {
 
   private final KingdomService kingdomService;
@@ -19,15 +22,17 @@ public class KingdomController {
     this.kingdomService = kingdomService;
   }
 
-  @GetMapping({"/kingdom","/kingdom/{id}"})
-  public ResponseEntity getKingdom(@PathVariable(required = false, value = "id") Integer kingdomId,
-                                   UsernamePasswordAuthenticationToken user) {
-    Kingdom kingdom = ((Player)user.getPrincipal()).getKingdom();
-    if (kingdomId == null || kingdomId == kingdom.getId()) {
-      return ResponseEntity.status(200).body(kingdomService.fetchKingdomData(kingdom));
-    } else {
-      return ResponseEntity.status(200).body(kingdomService.fetchKingdomData(kingdomId));
-    }
+  @GetMapping
+  public ResponseEntity<KingdomResFullDTO> getKingdom(Authentication user) {
+    return ResponseEntity
+        .status(200)
+        .body(kingdomService.fetchKingdomData(((Player)user.getPrincipal()).getKingdom()));
+  }
+
+  @GetMapping({"/{id}"})
+  public ResponseEntity<KingdomResWrappedDTO> getKingdom(
+      @PathVariable(required = false, value = "id") Integer kingdomId) {
+    return ResponseEntity.status(200).body(kingdomService.fetchKingdomData(kingdomId));
   }
 
 }
