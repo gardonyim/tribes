845f33b63ab2b48846f5e80a8c559888361a87d4
unknown <s.gabor1988@gmail.com>
2022-05-21 23:19:32 +0200

    fix(Put Building): fix tests of put endpoint

--- src/test/java/com/greenfoxacademy/springwebapp/building/BuildingServiceTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/building/BuildingServiceTest.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springwebapp.building;
 
+import com.greenfoxacademy.TestUtils;
 import com.greenfoxacademy.springwebapp.building.models.Building;
 import com.greenfoxacademy.springwebapp.building.models.BuildingDTO;
 import com.greenfoxacademy.springwebapp.building.models.BuildingType;
