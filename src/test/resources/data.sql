274d644e686724ab13d255475b9f981e36fc1c97
unknown <s.gabor1988@gmail.com>
2022-05-21 23:14:46 +0200

    refactor(Put Building): add new kingdom with buildings and resources into the testdb

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -9,23 +9,27 @@ INSERT INTO players (id, username, password) VALUES
     (1, 'existingtestuser', '$2a$10$b7wnqdlcepRbdn.PFKww8e11aZaLdZtOGl8RVNkd1mKFuWRzA/IpK'),
     (2, 'testuser2', '$2a$10$b7wnqdlcepRbdn.PFKww8e11aZaLdZtOGl8RVNkd1mKFuWRzA/IpK'),
     (3, 'testuser3', '$2a$10$b7wnqdlcepRbdn.PFKww8e11aZaLdZtOGl8RVNkd1mKFuWRzA/IpK'),
-    (4, 'testuser4', '$2a$10$b7wnqdlcepRbdn.PFKww8e11aZaLdZtOGl8RVNkd1mKFuWRzA/IpK');
+    (4, 'testuser4', '$2a$10$b7wnqdlcepRbdn.PFKww8e11aZaLdZtOGl8RVNkd1mKFuWRzA/IpK'),
+    (5, 'testuser5', '$2a$10$b7wnqdlcepRbdn.PFKww8e11aZaLdZtOGl8RVNkd1mKFuWRzA/IpK');
 
 INSERT INTO locations (id, x_coordinate, y_coordinate) VALUES
     (1, 0, 0),
     (2, 5, -5),
     (3, 5, -6),
-    (4, 11, -5);
+    (4, 11, -5),
+    (5, 11, -10);
 
 INSERT INTO kingdoms (id, name, player_id, location_id) VALUES
     (1, 'existingkingdom', 1, 1),
     (2, 'testkingdom2', 2, 2),
     (3, 'testkingdom3', 3, 3),
-    (4, 'testkingdom4', 4, 4);
+    (4, 'testkingdom4', 4, 4),
+    (5, 'testkingdom5', 5, 5);
 
 INSERT INTO resources (id, resource_type, amount, generation, updated_at, kingdom_id) VALUES
     (1, 'gold', 50, 0, '2022-05-01 18:48:42', 3),
-    (2, 'gold', 150, 0, '2022-05-01 18:48:42', 4);
+    (2, 'gold', 150, 0, '2022-05-01 18:48:42', 4),
+    (3, 'gold', 1000, 0, '2022-05-01 18:48:42', 5);
 
 INSERT INTO buildings (id, building_type, level, hp, kingdom_id, started_at, finished_at) VALUES
     (1, 'townhall', 1, 200, 1, '2022-05-01 18:48:42', '2022-05-01 18:48:42'),
