3d83f65dd6b518274736ef64961c24f06adf749e
unknown <s.gabor1988@gmail.com>
2022-05-25 13:12:12 +0200

    fix(Validation with annotation): fixies

--- src/main/java/com/greenfoxacademy/springwebapp/exceptions/ExceptionServiceImpl.java
+++ src/main/java/com/greenfoxacademy/springwebapp/exceptions/ExceptionServiceImpl.java
@@ -5,17 +5,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @Service
 public class ExceptionServiceImpl implements ExceptionService {
   @Override
   public ErrorDTO createInputParamsValidErrorMessage(MethodArgumentNotValidException e) {
-    StringBuilder sb = new StringBuilder();
+    StringBuilder errorDtoMessage = new StringBuilder();
+    List<String> errorMessages = new ArrayList<>();
     e.getBindingResult().getAllErrors().forEach((objectError -> {
       String fieldName = ((FieldError)objectError).getField();
       String errorMessage = objectError.getDefaultMessage();
-      sb.append(fieldName).append(": ").append(errorMessage).append(", ");
+      errorMessages.add(fieldName + ": " + errorMessage);
     }));
-    sb.substring(0, sb.length()-3);
-    return new ErrorDTO(sb.toString());
+    errorMessages.sort(Comparator.naturalOrder());
+    errorMessages.stream().forEach(em -> errorDtoMessage.append(em).append("; "));
+    return new ErrorDTO(errorDtoMessage.substring(0, errorDtoMessage.length() - 2));
   }
 }
