42bd9f71e79badceddd4c53e665732909574cb6e
unknown <s.gabor1988@gmail.com>
2022-06-01 14:49:04 +0200

    fix(Connect wit frontend)

--- src/main/java/com/greenfoxacademy/springwebapp/player/PlayerController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/player/PlayerController.java
@@ -5,7 +5,11 @@ import com.greenfoxacademy.springwebapp.player.models.RegistrationReqDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
