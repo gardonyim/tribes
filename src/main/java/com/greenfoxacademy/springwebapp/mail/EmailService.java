183005f8b20ffff554f2ac8535a19d01ad7f6817
Kond <peter.tamas@greenfoxacademy.com>
2022-06-02 12:44:07 +0200

    refactor(email): refactor email sending

--- src/main/java/com/greenfoxacademy/springwebapp/mail/EmailService.java
+++ src/main/java/com/greenfoxacademy/springwebapp/mail/EmailService.java
@@ -3,7 +3,6 @@ package com.greenfoxacademy.springwebapp.mail;
 import com.greenfoxacademy.springwebapp.exceptions.EmailSendingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -13,21 +12,22 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@PropertySource(value = {"classpath:application.properties"})
 public class EmailService {
-
-  @Value("${app.url}")
-  private String url;
-  @Value("${email.resource.path}")
-  private Resource resourceFile;
-  private String subject = "Sikeres regisztrálás";
 
   private final JavaMailSenderImpl javaMailSenderImpl;
   private final SpringTemplateEngine thymeleafTemplateEngine;
+
+  @Value("${email.resource.path}")
+  private Resource resourceFile;
+  @Value("${server.port}")
+  private Integer port;
+  private String subject = "Sikeres regisztrálás";
 
   @Autowired
   public EmailService(JavaMailSenderImpl javaMailSenderImpl, SpringTemplateEngine thymeleafTemplateEngine) {
@@ -38,7 +38,7 @@ public class EmailService {
   public void sendMessageUsingThymeleafTemplate(String to, String userName, String activationCode) {
     Map<String, Object> templateModel = new HashMap<>();
     templateModel.put("recipientName", userName);
-    templateModel.put("link", url + activationCode);
+    templateModel.put("link", getUrl() + activationCode);
 
     Context thymeleafContext = new Context();
     thymeleafContext.setVariables(templateModel);
@@ -51,16 +51,23 @@ public class EmailService {
     }
   }
 
+  public String getUrl() {
+    try {
+      return String.format("http://%s:%d/activation/", InetAddress.getLocalHost().getHostAddress(), port);
+    } catch (UnknownHostException e) {
+      throw new EmailSendingException();
+    }
+  }
+
   private String createPlainTextMessageBody(String activationCode) {
     return "Kedves Felhasználó!\n\n"
         + "Az alábbi linken tudod aktiválni profilodat:\n"
-        + "link: " + url + activationCode + " \n"
+        + "link: " + getUrl() + activationCode + " \n"
         + "\n\n Üdvözlettel: CH4-Tribes ";
   }
 
   private void sendHtmlMessage(String to, String subject, String htmlBody, String activationCode)
-      throws MessagingException {
-
+          throws MessagingException, UnknownHostException {
     MimeMessage message = javaMailSenderImpl.createMimeMessage();
     MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
     helper.setFrom(javaMailSenderImpl.getUsername());
